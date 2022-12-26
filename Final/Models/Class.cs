baa1f987ffe0320c3174fec5684652c1bb112a93
nbi67 <nbi67@NYNEO>
2022-12-26 20:32:41 +0700

    Add project files.

---
+++ Final/Models/Class.cs
@@ -0,0 +1,15 @@
+﻿namespace Final.Models
+{
+    public class Product
+    {
+        public string? productId { get; set; }
+
+        public string? productName { get; set; }
+
+        public string? categoryId { get; set; }
+
+        public string? price { get; set; }
+
+        
+    }
+}
